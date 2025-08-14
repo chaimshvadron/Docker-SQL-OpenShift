8e96966f10bb8e975333523edeed58ff64add323
ChaimShvadron <s0527649958@gmail.com>
2025-08-14 15:27:14 +0300

    add: create initial SQL scripts for database and data insertion

---
+++ data-loader/scripts/insert_data.sql
@@ -0,0 +1,14 @@
+USE data;
+
+INSERT INTO data (first_name, last_name) VALUES
+('David','Levi'),
+('Moshe','Cohen'),
+('Avi','Peretz'),
+('Yaakov','Mizrahi'),
+('Shimon','Avrahami');
+INSERT INTO data (first_name, last_name) VALUES
+('Yossi','Goldstein'),
+('Eli','Rosen'),
+('Ofer','Levy'),
+('Haim','Shwartz'),
+('Rami','Klein');
