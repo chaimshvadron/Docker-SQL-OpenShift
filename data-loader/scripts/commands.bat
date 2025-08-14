ec2ee0d026b903421250899108853af295b539d2
ChaimShvadron <s0527649958@gmail.com>
2025-08-14 16:06:03 +0300

    fix: update MySQL service configuration to use ClusterIP and remove NodePort; enhance commands for SQL script execution

--- data-loader/scripts/commands.bat
+++ data-loader/scripts/commands.bat
@@ -7,4 +7,17 @@ oc apply -f mysql-pvc.yaml
 oc apply -f mysql-deployment.yaml
 # Create a service for MySQL
 oc apply -f mysql-service.yaml
+# Get the MySQL pod
+oc get pod -n chaims-dev -l app=mysql
+# Copy the SQL scripts to the MySQL pod
+oc cp ..\..\scripts\create_data.sql  -n chaims-dev mysql-8489dbcf89-2cf4j:/tmp/create_data.sql
+# Copy the insert_data.sql script to the MySQL pod
+oc cp ..\..\scripts\insert_data.sql  -n chaims-dev mysql-8489dbcf89-2cf4j:/tmp/insert_data.sql
+# Execute the SQL scripts in the MySQL pod
+oc exec -it -n chaims-dev deploy/mysql -- bash
+# List the files in the /tmp directory
+ls -la /tmp
 
+# Execute the SQL scripts
+mysql -uroot -p"$MYSQL_ROOT_PASSWORD" < /tmp/create_data.sql
+mysql -uroot -p"$MYSQL_ROOT_PASSWORD" < /tmp/insert_data.sql
